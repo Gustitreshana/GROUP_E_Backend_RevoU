9620df9f92e3f8202f6fb297c1f99c839dff8e03
Gustitreshana <gustith2105@gmail.com>
2024-04-29 22:12:08 +0700

    refactor line 37

--- Zero_Hunger_API/app/__init__.py
+++ Zero_Hunger_API/app/__init__.py
@@ -34,7 +34,7 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get("PORT", 11463))
-    app.listen(host='0.0.0.0', port=port)
+    app.run(port=5000)
+
