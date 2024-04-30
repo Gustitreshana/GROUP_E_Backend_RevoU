130fe5b1b9be21180ab4a614ae1e8fbc22f7bd85
Gustitreshana <gustith2105@gmail.com>
2024-04-30 21:02:46 +0700

    update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:tMvaPMeZxjeNENRLoY
 
 # Initializing database
 init_db(app)
-jwt = JWTManager(app)
 
 # Setting JWT secret key directly
 app.config['JWT_SECRET_KEY'] = "CQbWLxP5s6Wah6NWHz6ElSMxpbJnVD6BTX2CimbnzfyKQU7dNfDLGg8k6eMVJ1h8AY0hQu3fCpK9YvTFzwNfIsHCZ_ckUm-WPmre4t0KEwqx577w8WPvl_IKjfof5qJDJgJthdcL6cBDb5x-CRGoArjA60ukIKeO-S2v7suOmg6YPllB1PWzK89r0jyJT9vxiX6ANcwOJEIijrQpLpFSdyGeHvHx3_ulbQjtzLrOZdImCW6FAhtYGBBQyteMeofXpoUgDGhFOJv2cHt9_ILhvCAo1iNLlZ3HobQUGnpl-wYlrUWWiNsdFTi2cPBW-Oo6Ys-jP-FCfkAFJTnFLCkRfw"
+jwt = JWTManager(app)
 
 # Registering blueprints
 app.register_blueprint(user_routes)
