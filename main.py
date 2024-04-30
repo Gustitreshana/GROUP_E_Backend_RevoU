f6e9b4f27cbb8d62d556e96a1e6ec6676af603f1
Gustitreshana <gustith2105@gmail.com>
2024-04-30 21:17:46 +0700

    update main.py

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from flask_jwt_extended import JWTManager
 from utils.db import init_db
 from controllers.user_management_route import user_routes
 from controllers.contact_management_route import contact_routes
+from dotenv import load_dotenv
+import os
 
 # Initializing Flask application
 app = Flask(__name__)
 
+load_dotenv()
+
 # Setting database URI directly
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:tMvaPMeZxjeNENRLoYltgzgqtAslgdDa@monorail.proxy.rlwy.net:11463/zero_hunger"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 # Initializing database
 init_db(app)
 
 # Setting JWT secret key directly
-app.config['JWT_SECRET_KEY'] = "CQbWLxP5s6Wah6NWHz6ElSMxpbJnVD6BTX2CimbnzfyKQU7dNfDLGg8k6eMVJ1h8AY0hQu3fCpK9YvTFzwNfIsHCZ_ckUm-WPmre4t0KEwqx577w8WPvl_IKjfof5qJDJgJthdcL6cBDb5x-CRGoArjA60ukIKeO-S2v7suOmg6YPllB1PWzK89r0jyJT9vxiX6ANcwOJEIijrQpLpFSdyGeHvHx3_ulbQjtzLrOZdImCW6FAhtYGBBQyteMeofXpoUgDGhFOJv2cHt9_ILhvCAo1iNLlZ3HobQUGnpl-wYlrUWWiNsdFTi2cPBW-Oo6Ys-jP-FCfkAFJTnFLCkRfw"
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
 # Registering blueprints
