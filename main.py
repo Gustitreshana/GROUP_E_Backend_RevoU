d1578ed7f892bcd4a6141a181cfc6482c5787378
Gustitreshana <gustith2105@gmail.com>
2024-05-01 21:55:30 +0700

    add CORS main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from utils.db import init_db
 from controllers.user_management_route import user_routes
 from controllers.contact_management_route import contact_routes
 from dotenv import load_dotenv
+from flask_cors import CORS
 import os
 
 # Initializing Flask application
 app = Flask(__name__)
+
+CORS(app)
 
 load_dotenv()
 
@@ -27,8 +30,8 @@ app.register_blueprint(contact_routes)
 
 # Defining routes here
 @app.route('/')
-def home():
-    return jsonify({"message": "Hello, Zero Hunger API!"}), 200
+def index():
+    return jsonify({'message': 'Hello, CORS is enabled!'})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
