7456199c641ac451fc0856d9478ad6e602e1189f
Gustitreshana <gustith2105@gmail.com>
2024-05-06 22:06:38 +0700

    add blueprint for Profile route

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager
 from utils.db import init_db
 from controllers.user_management_route import user_routes
 from controllers.contact_management_route import contact_routes
+from controllers.profile_management_route import profile_routes
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
@@ -27,6 +28,7 @@ jwt = JWTManager(app)
 # Registering blueprints
 app.register_blueprint(user_routes)
 app.register_blueprint(contact_routes)
+app.register_blueprint(profile_routes)
 
 # Defining routes here
 @app.route('/')
