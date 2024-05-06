95018f31a4778692800d06e2a855b32db4348430
Gustitreshana <gustith2105@gmail.com>
2024-05-06 22:07:39 +0700

    realation one-to-one with table users

--- models/user_model.py
+++ models/user_model.py
@@ -11,6 +11,8 @@ class User(db.Model):
     created_at = db.Column(DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
     
+    profile = db.relationship('Profile', backref='user', uselist=False, lazy=True)
+
     def to_dict(self):
         return {
             'id': self.id,
