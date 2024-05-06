302e0c1749eec121ff293199eb3ac549a117a988
Gustitreshana <gustith2105@gmail.com>
2024-05-06 23:02:53 +0700

    add dict

--- models/profile_model.py
+++ models/profile_model.py
@@ -13,5 +13,16 @@ class Profile(db.Model):
     created_at = db.Column(DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(DateTime(timezone=True), onupdate=func.now())
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'realname': self.realname,
+            'address': self.address,
+            'occupation': self.occupation,
+            'user_id': self.user_id,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
     def __repr__(self):
         return f"<Profile {self.id} for user {self.user_id}>"
